1385461ff05f406055432b13dfe81c66a5e2bcde
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-27 18:59:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,5 +14,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  
+
 end
